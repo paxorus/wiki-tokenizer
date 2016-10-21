8c8ad6aafd8d20fff71ee0770274d81e9b71a66e
paxorus <prakhar@brandeis.edu>
2016-10-21 00:12:19 -0400

    Tokenizer main boilerplate

--- src/main/java/code/lemma/TokenizerMain.java
+++ src/main/java/code/lemma/TokenizerMain.java
@@ -1,11 +1,28 @@
 package code.lemma;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class TokenizerMain {
 
 	public static void main(String[] args) {
+		List<String> articles = getArticles();
+        Tokenizer t = new Tokenizer();
 
-		System.out.println(new Tokenizer().tokenize(sentence));
+
+		System.out.println(t.tokenize(articles.get(0)));
+	}
+
+	public static List<String> getArticles() {
+		List<String> articles = new ArrayList<String>();
+        ClassLoader cl = this.getClass().getClassLoader();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(cl.getResourceAsStream("articles.txt")));
+
+        // iterate through file, add names
+        String line;
+        while ((line = reader.readLine()) != null) {
+      	  articles.add(qualifyName(line));
+        }
+        return articles;
 	}
 }

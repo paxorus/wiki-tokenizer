d600ca8315566014d05d7bc811a28714c13308f7
isaacmg <igodfried@isaac26.com>
2016-10-21 03:20:21 -0400

    fixed bug and comments
    Fixed small bug regarding number and added some additional comments.
    Updated format

--- src/main/java/code/lemma/Tokenizer.java
+++ src/main/java/code/lemma/Tokenizer.java
@@ -3,63 +3,65 @@ package code.lemma;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Tokenizer {
 	ArrayList<String> stopWords;
-	ArrayList<String> tokens; 
-	
+	ArrayList<String> tokens;
 
 	public Tokenizer() {
-		// TODO Auto-generated constructor stub
-		//Manually adding elements temporarily. In final probably want to read in stop words from file and split.
+
+		// Manually adding elements temporarily. In final probably want to read
+		// in stop words from file and add them in loop.
 		stopWords = new ArrayList<String>();
 		tokens = new ArrayList<String>();
 		stopWords.add("a");
 		stopWords.add("the");
-		//etc etc
+		stopWords.add("at");
+		// etc etc
 	}
 
 	public List<String> tokenize(String sentence) {
-		// TODO implement your tokenizing code here
+		// TODO Implement the lemmatizer and get a complete list of stop words
+
 		String removedString = removeNonLetters(sentence);
 		removedString = removedString.toLowerCase();
 		String toks[] = removedString.split("\\s+");
-		for(String word : toks){
-			if(!stopWords.contains(word)){
-				//tokens.add(lemma(word));
+		for (String word : toks) {
+			if (!stopWords.contains(word)) {
+				// tokens.add(lemma(word));
 				tokens.add(word);
-				
+
 			}
-			
+
 		}
-		System.out.println(removedString);
+
 		return tokens;
 	}
-	public String removeNonLetters(String sentence){
+
+	public String removeNonLetters(String sentence) {
 		StringBuffer s = new StringBuffer();
-		for (int i=0 ; i < sentence.length(); i++) {
-			char c= sentence.charAt(i);
-	        if(Character.isLetter(c)) {
-	        	s.append(c);
-	        	}
-	        else if(c >= '0' && c <= '9'){
-	        	//If number is next to letter we assume we want to keep that number as part of word i.e 4chan
-	        	if(Character.isLetter(sentence.charAt(i+1))){
-	        		s.append(c);
-	        	}
-	        }
-	        else if(c==' '){
-	        	s.append(c);
-	        }
-	        else{
-	        }
+		for (int i = 0; i < sentence.length(); i++) {
+			char c = sentence.charAt(i);
+			if (Character.isLetter(c)) {
+				s.append(c);
+			} else if (c >= '0' && c <= '9') {
+				// If number is next to letter we assume we want to keep that
+				// number as part of word i.e 4chan. Requires additional i+1
+				// check in order to not throw error.
+				if (i + 1 < sentence.length()) {
+					if (Character.isLetter(sentence.charAt(i + 1))) {
+						s.append(c);
+					}
+				}
+			} else if (c == ' ') {
+				s.append(c);
+			}
 		}
-	   return s.toString();
+		return s.toString();
 	}
-		public String lemma(String word)
-		{ //TODO implement lemmatization here
-			return word;
-		}
-	
-		
+
+	public String lemma(String word) { // TODO implement lemmatization here
+
+		return word;
+	}
+
 }
